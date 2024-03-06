5887896367b23437e81957af9db4a674234efb71
Kevin Cabral <kevin1tdsb@gmail.com>
2024-03-06 16:53:05 -0300

    Create chat functions and styles. Also static files

--- chat/settings.py
+++ chat/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.chat_app.apps.ChatAppConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'chat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates', 
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -115,7 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ static/js/script.js
@@ -0,0 +1,68 @@
+const chatMessages = Qs(".messages");
+
+const setRoomActive = (room_id) => {
+  QsAll(".list-rooms li").forEach((el) => el.classList.remove("active"));
+  Qs(`#room-${room_id}`).classList.add("active");
+  Qs("#selected-room").value = room_id;
+};
+
+const getMessages = async (room_id) => {
+  const response = await fetch(`/chat/${room_id}`);
+  const html = await response.text();
+  chatMessages.innerHTML = html;
+  setRoomActive(room_id);
+};
+
+const sendMessage = async (data) => {
+  const response = await fetch(`/chat/${data.room_id}/send`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": data.csrfmiddlewaretoken,
+    },
+    body: JSON.stringify(data),
+  });
+  const html = await response.text();
+  const uniqueMessageContainer = Qs(".unique-message");
+  uniqueMessageContainer.insertAdjacentHTML("beforeend", html);
+  Qs("form").reset();
+};
+
+const createRoom = async (data) => {
+  const response = await fetch(`/chat/create-room`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": data.csrfmiddlewaretoken,
+    },
+    body: JSON.stringify(data),
+  });
+  const html = await response.text();
+  const listRooms = Qs(".list-rooms");
+  listRooms.insertAdjacentHTML("afterbegin", html);
+  const modal = bootstrap.Modal.getInstance(Qs(".modal"));
+  Qs(".create-room").reset();
+  modal.hide();
+  getLastRoom();
+};
+
+const getLastRoom = () => {
+  Qs(".list-rooms li").click();
+};
+////Events
+//intercept sent message form
+Qs(".send-message").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(e.target).entries());
+  sendMessage(data);
+});
+
+//intercept create new room
+Qs(".create-room").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(e.target).entries());
+  createRoom(data);
+});
+
+//INIT
+getLastRoom();
